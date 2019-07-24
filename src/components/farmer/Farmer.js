a6342cd60396b36304bf5a24047b50ff3b5586e7
Pranav Singhal <pranavsinghal96@gmail.com>
2019-07-24 15:38:44 +0530

    added react-bootstrap

---
+++ src/components/farmer/Farmer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Container from "react-bootstrap/Container";
+
+const Farmer = () => {
+    return (
+        <Container fluid={true}>
+
+        </Container>
+    );
+};
+
+export default Farmer;
