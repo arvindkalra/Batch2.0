6c7cf740c77074c56a8fa8b0c02b0be04a909d8b
Pranav Singhal <pranavsinghal96@gmail.com>
2019-07-29 15:53:32 +0530

    added products component

---
+++ src/components/farmer/Products.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Row from "react-bootstrap/Row";
+
+const Products = () => {
+    return (
+        <Row>
+            
+        </Row>
+    );
+};
+
+export default Products;
