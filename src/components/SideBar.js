bce6036f4a03228b9b46b36445d301f1837ca841
Pranav Singhal <pranavsinghal96@gmail.com>
2019-09-03 14:01:05 +0530

    made sidebar responsive

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -5,7 +5,7 @@ import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 import { Link } from "react-router-dom";
 
-const SideBar = ({ closeSideBar, userName, profileImage, profession }) => {
+const SideBar = ({ closeSideBar, userName, profileImage, profession, hidden }) => {
   const getProductListLink = () => {
     switch (profession) {
       case "cultivator":
@@ -16,8 +16,8 @@ const SideBar = ({ closeSideBar, userName, profileImage, profession }) => {
   };
 
   return (
-    <Container>
-      <div className={"sidebar"}>
+    <Container >
+      <div className={hidden}>
         <header>
           <button
             type="button"
@@ -27,13 +27,13 @@ const SideBar = ({ closeSideBar, userName, profileImage, profession }) => {
               closeSideBar();
             }}
           >
-            <span aria-hidden="true">&times;</span>
+            &times;
           </button>
           <h1 className={"title"}> BATCH</h1>
         </header>
         <section className={"sidebar-profile-section"}>
           <Row>
-            <Col md={5}>
+            <Col md={5} sm={5} xs={5} >
               <img
                 src={
                   profileImage ||
@@ -44,13 +44,13 @@ const SideBar = ({ closeSideBar, userName, profileImage, profession }) => {
                 className={"profile-image"}
               />
             </Col>
-            <Col md={7} className={"sidebar-profile-details"}>
+            <Col md={7} sm={7} xs={7}  className={"sidebar-profile-details"}>
               <span className={"name"}>
                 {" "}
                 {userName || localStorage.getItem("name") || "Loading..."}
               </span>{" "}
               <br />
-              <span>{profession.toUpperCase() || "Loading..."}</span>
+              <span className={'profession'}>{profession.toUpperCase() || "Loading..."}</span>
             </Col>
           </Row>
         </section>
