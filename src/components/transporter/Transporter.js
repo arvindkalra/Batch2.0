66c8962e2a4d0eb5a010d5ef062b47cf4e703390
arvindkalra <kalarvind97@gmail.com>
2019-09-11 14:08:11 +0530

    redirect to about page if used details not found on transporter page

--- src/components/transporter/Transporter.js
+++ src/components/transporter/Transporter.js
@@ -1,37 +1,43 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 import Layout from "../Layout";
 import "../../assets/stylesheets/transporter.scss";
-import {connectToWeb3} from "../../dbController/init";
-import {getTransporterDetails} from "../../dbController/transporterRole";
-import {clearLocal} from "../../helpers";
+import { connectToWeb3 } from "../../dbController/init";
+import { getTransporterDetails } from "../../dbController/transporterRole";
+import { clearLocal } from "../../helpers";
 import NewDashboard from "./newDashboard";
 import TransporterDashboardUnused from "./TransporterDashboard (unused)";
 
-const Transporter = ({location, history}) => {
-    const [userName, setUserName] = useState('');
-    const [profileImage, setProfileImage] = useState('');
-    useEffect(() => {
-        connectToWeb3().then(() => {
-            clearLocal();
+const Transporter = ({ location, history, userRole }) => {
+  const [userName, setUserName] = useState("");
+  const [profileImage, setProfileImage] = useState("");
+  useEffect(() => {
+    connectToWeb3().then(() => {
+      clearLocal();
 
-            getTransporterDetails().then((obj) => {
-
-                setUserName(obj.name);
-                localStorage.setItem('name', obj.name);
-                setProfileImage(obj.profileImage);
-                localStorage.setItem('profileImage', obj.profileImage)
-
-            })
-        })
+      getTransporterDetails().then(obj => {
+        if (typeof obj !== "undefined") {
+          setUserName(obj.name);
+          localStorage.setItem("name", obj.name);
+          setProfileImage(obj.profileImage);
+          localStorage.setItem("profileImage", obj.profileImage);
+        } else {
+          history.push(`/${userRole}/about`);
+        }
+      });
     });
+  });
 
-    return (
-        <Layout userName={userName} profileImage={profileImage} location={location} history={history}>
-            {/*<TransporterDashboard location={location}/>*/}
-            <NewDashboard location={location}/>
-
-        </Layout>
-    );
+  return (
+    <Layout
+      userName={userName}
+      profileImage={profileImage}
+      location={location}
+      history={history}
+    >
+      {/*<TransporterDashboard location={location}/>*/}
+      <NewDashboard location={location} />
+    </Layout>
+  );
 };
 
 export default Transporter;
