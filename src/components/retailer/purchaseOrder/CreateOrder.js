08f01c37f9b01f4f14d4b2d9e81959ed6d624c11
Pranav Singhal <pranavsinghal96@gmail.com>
2019-09-17 14:08:29 +0530

    updated warning for purchase order

--- src/components/retailer/purchaseOrder/CreateOrder.js
+++ src/components/retailer/purchaseOrder/CreateOrder.js
@@ -1,142 +1,152 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import Layout from "../../Layout";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import { setBreadcrumb } from "../../../helpers";
+import {setBreadcrumb} from "../../../helpers";
 import "../../../assets/stylesheets/App.scss";
 
 import OrderTable from "./OrderTable";
-import { Button } from "react-bootstrap";
+import {Button} from "react-bootstrap";
 import Modal from "react-bootstrap/Modal";
 import ConfirmOrder from "./ConfirmOrder";
-import { connectToWeb3 } from "../../../dbController/init";
+import {connectToWeb3} from "../../../dbController/init";
 import {
-  fetchRowsForCreatingPurchaseOrder,
-  getRetailerDetails
+    fetchRowsForCreatingPurchaseOrder,
+    getRetailerDetails
 } from "../../../dbController/retailerRole";
 import Loader from "../../Loader";
-import { getDistributorDetails } from "../../../dbController/distributorRole";
+import {getDistributorDetails} from "../../../dbController/distributorRole";
 
-const CreateOrder = ({ location, history }) => {
-  const [grandTotal, setGrandTotal] = useState(0);
-  const [showModal, setShowModal] = useState(false);
-  const [loader, setLoader] = useState(true);
-  const [productList, setProductList] = useState([]);
-  const [retailerDetails, setRetailerDetails] = useState({});
+const CreateOrder = ({location, history}) => {
+    const [grandTotal, setGrandTotal] = useState(0);
+    const [showModal, setShowModal] = useState(false);
+    const [loader, setLoader] = useState(true);
+    const [productList, setProductList] = useState([]);
+    const [retailerDetails, setRetailerDetails] = useState({});
 
-  useEffect(() => {
-    connectToWeb3().then(() => {
-      let numRows = 0;
-      let tempProductList = productList;
-      fetchRowsForCreatingPurchaseOrder(
-        "0x7949173E38cEf39e75E05D2d2C232FBE8BAe5E20",
-        (row, total) => {
-          if (!row) {
-            setLoader(false);
-            return;
-          }
-          numRows++;
-          if (row.currentState.value !== 5) {
-            if (numRows === total) {
-              setLoader(false);
-            }
-            return;
-          }
-          getDistributorDetails(row.details.distributorAddress).then(
-            ({ companyName }) => {
-              let oldUnitsUsed = row.details.totalPacketsUsed
-                ? row.details.totalPacketsUsed
-                : 0;
-              let left = row.details.totalPacketsManufactured - oldUnitsUsed;
-              if(left > 0){
-                  let obj = {
-                      id: row.uid,
-                      productName: row.details.productName,
-                      distributor: companyName,
-                      distributorAddress: row.details.distributorAddress,
-                      availableQuantity: left,
-                      price: row.details.distributorToRetailerPrice
-                  };
-                  tempProductList.push(obj);
-                  setProductList([...tempProductList]);
-              }
-              if (numRows === total) {
-                setLoader(false);
-              }
-            }
-          );
-        }
-      );
-      getRetailerDetails().then(setRetailerDetails);
-    });
-  }, []);
+    useEffect(() => {
+        connectToWeb3().then(() => {
+            let numRows = 0;
+            let tempProductList = productList;
+            fetchRowsForCreatingPurchaseOrder(
+                "0x7949173E38cEf39e75E05D2d2C232FBE8BAe5E20",
+                (row, total) => {
+                    if (!row) {
+                        setLoader(false);
+                        return;
+                    }
+                    numRows++;
+                    if (row.currentState.value !== 5) {
+                        if (numRows === total) {
+                            setLoader(false);
+                        }
+                        return;
+                    }
+                    getDistributorDetails(row.details.distributorAddress).then(
+                        ({companyName}) => {
+                            let oldUnitsUsed = row.details.totalPacketsUsed
+                                ? row.details.totalPacketsUsed
+                                : 0;
+                            let left = row.details.totalPacketsManufactured - oldUnitsUsed;
+                            if (left > 0) {
+                                let obj = {
+                                    id: row.uid,
+                                    productName: row.details.productName,
+                                    distributor: companyName,
+                                    distributorAddress: row.details.distributorAddress,
+                                    availableQuantity: left,
+                                    price: row.details.distributorToRetailerPrice
+                                };
+                                tempProductList.push(obj);
+                                setProductList([...tempProductList]);
+                            }
+                            if (numRows === total) {
+                                setLoader(false);
+                            }
+                        }
+                    );
+                }
+            );
+            getRetailerDetails().then(setRetailerDetails);
+        });
+    }, []);
 
-  const updateProduct = (index, key, value) => {
-    let newProductList = productList;
-    newProductList[index][key] = value;
-    setProductList(newProductList);
+    const updateProduct = (index, key, value) => {
+        let newProductList = productList;
+        newProductList[index][key] = value;
+        setProductList(newProductList);
 
-    // setProductList(newProductList)
-  };
+        // setProductList(newProductList)
+    };
 
-  return (
-    <>
-      <Layout location={location} history={history}>
-        <Row>
-          <Col>{setBreadcrumb(location.pathname)}</Col>
-        </Row>
-        <Row>
-          <Col>
-            <section className={"table-section card"}>
-              <OrderTable
-                productList={productList}
-                updateProduct={(index, key, value) => {
-                  updateProduct(index, key, value);
-                }}
-                setGrandTotal={arr => {
-                  setGrandTotal(arr.reduce((a, b) => a + b, 0));
-                }}
-              />
-              <Row>
-                <Col md={12} className={"text-right"}>
-                  <Button
-                    onClick={() => {
-                      if (
-                        productList.filter(product => {
-                          return product.orderedAmount;
-                        }).length > 0
-                      )
-                        setShowModal(true);
-                    }}
-                  >
-                    Create Order
-                  </Button>
-                  <Modal
-                    size={"xl"}
-                    show={showModal}
-                    onHide={() => {
-                      setShowModal(false);
-                    }}
-                  >
-                    <ConfirmOrder
-                      productList={productList.filter(product => {
-                        return product.orderedAmount;
-                      })}
-                      header={retailerDetails}
-                      closeModal={() => setShowModal(false)}
-                      isUsedOnConfirm={true}
-                      canPlace={false}
-                    />
-                  </Modal>
-                </Col>
-              </Row>
-            </section>
-          </Col>
-        </Row>
-      </Layout>
-      {loader ? <Loader message={"Fetching Data"} /> : null}
-    </>
-  );
+    return (
+        <>
+            <Layout location={location} history={history}>
+                <Row>
+                    <Col>{setBreadcrumb(location.pathname)}</Col>
+                </Row>
+                <Row>
+                    <Col md={12}>
+                        <div className="utils__title ">
+                            <strong className="text-uppercase ">Create a New Purchase Order</strong>
+                        </div>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <section className={"table-section card"}>
+                            <div className="utils__title ">
+                                <strong className="text-uppercase ">Available Products</strong>
+                            </div>
+                            <OrderTable
+                                productList={productList}
+                                updateProduct={(index, key, value) => {
+                                    updateProduct(index, key, value);
+                                }}
+                                setGrandTotal={arr => {
+                                    setGrandTotal(arr.reduce((a, b) => a + b, 0));
+                                }}
+                            />
+                            <Row>
+                                <Col md={12} className={"text-right"}>
+                                    <Button
+                                        onClick={() => {
+                                            if (
+                                                productList.filter(product => {
+                                                    return product.orderedAmount;
+                                                }).length > 0
+                                            )
+                                                setShowModal(true);
+                                        }}
+                                    >
+                                        Create Purchase Order
+                                    </Button>
+                                    <Modal
+                                        size={"xl"}
+                                        show={showModal}
+                                        onHide={() => {
+                                            setShowModal(false);
+                                        }}
+                                    >
+                                        <ConfirmOrder
+                                            productList={productList.filter(product => {
+                                                return product.orderedAmount;
+                                            })}
+                                            header={retailerDetails}
+                                            closeModal={() => setShowModal(false)}
+                                            isUsedOnConfirm={true}
+                                            canPlace={false}
+                                        />
+                                    </Modal>
+                                </Col>
+                            </Row>
+                        </section>
+                    </Col>
+                </Row>
+            </Layout>
+            {loader ? <Loader message={"Fetching Data"}/> : null}
+        </>
+    );
 };
 
 export default CreateOrder;
