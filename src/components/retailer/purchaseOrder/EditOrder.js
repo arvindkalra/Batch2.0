bbd7869b03c86c74fb931fe895d894b0a5f92037
arvindkalra <kalarvind97@gmail.com>
2019-09-17 18:02:03 +0530

    added edit order for retailer

--- src/components/retailer/purchaseOrder/EditOrder.js
+++ src/components/retailer/purchaseOrder/EditOrder.js
@@ -1,16 +1,22 @@
 import React, { useEffect, useState } from "react";
 import Layout from "../../Layout";
 import Col from "react-bootstrap/Col";
-import { makeXHR, setBreadcrumb } from "../../../helpers";
+import {
+  createPurchaseOrderId,
+  makeXHR,
+  setBreadcrumb
+} from "../../../helpers";
 import Row from "react-bootstrap/Row";
 import { connectToWeb3, convertFromHex } from "../../../dbController/init";
-import {Card, Form, Table} from "react-bootstrap";
+import { Card, Form, Table } from "react-bootstrap";
 import { fetchRowsForCreatingPurchaseOrder } from "../../../dbController/retailerRole";
 import { getDistributorDetails } from "../../../dbController/distributorRole";
 import EditRow from "./EditRow";
+import Loader from "../../Loader";
 
 const EditOrder = ({ history, location, match }) => {
-  const [orderList, setOrderList] = useState({orders:[]});
+  const [orderList, setOrderList] = useState({ orders: [] });
+  const [loader, setLoader] = useState(true);
 
   const orderId = match.params.id;
   useEffect(() => {
@@ -45,8 +51,9 @@ const EditOrder = ({ history, location, match }) => {
                 : 0;
               x.availableUnits =
                 row.details.totalPacketsManufactured - oldUnitsUsed;
-              x.price = row.details.distributorToRetailerPrice
-                x.productName = row.details.productName
+              x.price = row.details.distributorToRetailerPrice;
+              x.productName = row.details.productName;
+              x.url = createPurchaseOrderId(orderId, x.orderNumber);
 
               if (totalFetched === total) {
                 setOrderList(result);
@@ -59,44 +66,54 @@ const EditOrder = ({ history, location, match }) => {
   }, []);
 
   function doPendingJob(orderList) {
+    setLoader(false);
     console.log(orderList);
   }
 
   return (
-    <Layout history={history} location={location}>
-      <Row>
-        <Col>{setBreadcrumb(location.pathname)}</Col>
-      </Row>
-      <Row>
-        <Col>
-          <section className={"table-section card"}>
+    <>
+      <Layout history={history} location={location}>
+        <Row>
+          <Col>{setBreadcrumb(location.pathname)}</Col>
+        </Row>
+        <Row>
+          <Col>
+            <section className={"table-section card"}>
               <Card.Header>
-                  <strong className={"utils__title"}>Edit Purchase Order</strong>
+                <strong className={"utils__title"}>Edit Purchase Order</strong>
               </Card.Header>
-            <Table>
-              <thead>
-                <tr>
-                  <th>Product Id</th>
-                  <th>Product Name</th>
-                  <th>Distributor</th>
-                  <th>Quantity</th>
-                  <th>Available</th>
-                  <th>Price( $ x.xx / unit)</th>
-                  <th>Total</th>
-                </tr>
-              </thead>
+              <Table>
+                <thead>
+                  <tr>
+                    <th>Product Id</th>
+                    <th>Product Name</th>
+                    <th>Distributor</th>
+                    <th>Quantity</th>
+                    <th>Available</th>
+                    <th>Price( $ x.xx / unit)</th>
+                    <th>Total</th>
+                  </tr>
+                </thead>
 
                 <tbody>
-                {orderList.orders.map((order,index) => {
-                    return(<EditRow key={index} order={order} index={index} distributorName={orderList.distributorName} />)
-                })}
-
+                  {orderList.orders.map((order, index) => {
+                    return (
+                      <EditRow
+                        key={index}
+                        order={order}
+                        index={index}
+                        distributorName={orderList.distributorName}
+                      />
+                    );
+                  })}
                 </tbody>
-            </Table>
-          </section>
-        </Col>
-      </Row>
-    </Layout>
+              </Table>
+            </section>
+          </Col>
+        </Row>
+      </Layout>
+      {loader ? <Loader message={"Fetching Details..."} /> : null}
+    </>
   );
 };
 
--- src/components/retailer/purchaseOrder/EditRow.js
+++ src/components/retailer/purchaseOrder/EditRow.js
@@ -1,92 +1,98 @@
-import React, {useState} from 'react';
-import {Button, Form, FormControl} from "react-bootstrap";
+import React, { useState } from "react";
+import { Button, Form, FormControl } from "react-bootstrap";
+import { makeXHR, parsePurchaseOrderId } from "../../../helpers";
+import { connectToWeb3 } from "../../../dbController/init";
+import { OWN_ADDRESS } from "../../../dbController/Web3Connections";
 
-const EditRow = ({index, order, distributorName}) => {
-    const [total, setTotal] = useState(parseFloat(order.amount) * parseFloat(order.price));
-    const [amount, setAmount] = useState(order.amount);
-    const [disabled, setDisabled] = useState(true);
-    const [updateBtnDisabled, setUpdateBtnDisabled] = useState(true);
-    const [isInvalid, setIsInvalid] = useState(false);
+const EditRow = ({ index, order, distributorName }) => {
+  const [total, setTotal] = useState(
+    parseFloat(order.amount) * parseFloat(order.price)
+  );
+  const [amount, setAmount] = useState(order.amount);
+  const [disabled, setDisabled] = useState(true);
+  const [updateBtnDisabled, setUpdateBtnDisabled] = useState(true);
+  const [isInvalid, setIsInvalid] = useState(false);
 
+  const handleUpdate = e => {
+    makeUpdateRequest({ url: order.url, amount }).then(() => {
+      window.location.reload();
+    });
+  };
 
-    const handleUpdate = e => {
-        console.log(e)
-    };
+  const makeUpdateRequest = ({ url, amount }) => {
+    return connectToWeb3().then(() => {
+      let retailerAddress = OWN_ADDRESS;
+      let [purchaseOrderId, orderId] = parsePurchaseOrderId(url);
+      return makeXHR("PATCH", "editOrder", {
+        retailerAddress,
+        purchaseOrderId,
+        orderId,
+        amount
+      });
+    });
+  };
 
+  const handleChange = e => {
+    let val = e.target.value;
 
-    const handleChange = e => {
-        let val = e.target.value;
+    if (val !== "") {
+      val = parseFloat(e.target.value);
+      if (val > parseFloat(order.availableUnits)) {
+        console.log("here");
+        setIsInvalid(true);
+        setUpdateBtnDisabled(true);
+      } else {
+        setIsInvalid(false);
+        setUpdateBtnDisabled(false);
+      }
 
-        if (val !== "") {
+      if (val === parseFloat(order.amount)) {
+        setUpdateBtnDisabled(true);
+      }
+      setAmount(val);
+      setTotal(val * parseFloat(order.price));
+    } else {
+      setAmount("");
+      setTotal(0);
+    }
+  };
 
-            val = parseFloat(e.target.value);
-            if (val > parseFloat(order.availableUnits)) {
-                console.log("here")
-                setIsInvalid(true);
-                setUpdateBtnDisabled(true)
-            } else {
-                setIsInvalid(false);
-                setUpdateBtnDisabled(false)
-            }
+  return (
+    <tr key={index}>
+      <td>{order.url}</td>
+      <td>{order.productName}</td>
+      <td>{distributorName}</td>
+      <td>
+        <Form.Group>
+          <Form.Control
+            type={"number"}
+            value={amount}
+            onChange={handleChange}
+            disabled={disabled}
+            isInvalid={isInvalid}
+          />
+          <FormControl.Feedback type={"invalid"}>
+            <strong>Amount cant exceed available quantity</strong>
+          </FormControl.Feedback>
+          <Button
+            onClick={() => {
+              setDisabled(false);
+            }}
+          >
+            Edit
+          </Button>
 
-            if (val === parseFloat(order.amount)) {
-                setUpdateBtnDisabled(true)
-            }
-            setAmount(val);
-            setTotal(val * parseFloat(order.price))
-
-        } else {
-
-            setAmount('');
-            setTotal(0)
-        }
-
-
-    };
-
-    return (
-        <tr key={index}>
-            <td>
-                {order.productUnitId}
-            </td>
-            <td>
-                {order.productName}
-            </td>
-            <td>
-                {distributorName}
-            </td>
-            <td>
-                <Form.Group>
-                    <Form.Control type={'number'} value={amount}
-                                  onChange={handleChange}
-                                  disabled={disabled}
-                                  isInvalid={isInvalid}
-                    />
-                    <FormControl.Feedback type={"invalid"}>
-                        <strong>
-                            Amount cant exceed available quantity
-                        </strong>
-                    </FormControl.Feedback>
-                    <Button onClick={() => {
-                        setDisabled(false)
-                    }}>Edit</Button>
-
-                    <Button disabled={updateBtnDisabled} onClick={handleUpdate}>Update</Button>
-
-                </Form.Group>
-                {/*{order.amount}*/}
-            </td>
-            <td>
-                {order.availableUnits}
-            </td>
-            <td>
-                {order.price}
-            </td>
-            <td>
-                {total}
-            </td>
-        </tr>
-    );
+          <Button disabled={updateBtnDisabled} onClick={handleUpdate}>
+            Update
+          </Button>
+        </Form.Group>
+        {/*{order.amount}*/}
+      </td>
+      <td>{order.availableUnits}</td>
+      <td>{order.price}</td>
+      <td>{total}</td>
+    </tr>
+  );
 };
 // edit button enabled
 // when form value changes from original to sth else, update enabled
