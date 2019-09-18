6b41dfcce2bd38b891469437d4453a2fdf266c76
arvindkalra <kalarvind97@gmail.com>
2019-09-18 12:37:05 +0530

    added api calls for distributor purchase order page

--- src/components/distributor/PurchaseOrder.js
+++ src/components/distributor/PurchaseOrder.js
@@ -1,47 +1,93 @@
-import React,{useEffect} from 'react';
+import React, { useEffect, useState } from "react";
 import Layout from "../Layout";
 import Col from "react-bootstrap/Col";
-import {makeXHR, setBreadcrumb} from "../../helpers";
+import { makeXHR, setBreadcrumb } from "../../helpers";
 import Row from "react-bootstrap/Row";
-import {Table} from "react-bootstrap";
-import config from "../../config";
+import { Table } from "react-bootstrap";
+import { connectToWeb3, convertFromHex } from "../../dbController/init";
+import { getRetailerDetails } from "../../dbController/retailerRole";
+import { fetchProductUnitDetailsUsingUID } from "../../dbController/manufacturerRole";
+import Loader from "../Loader";
 
-const PurchaseOrder = ({history,location,match}) => {
+const PurchaseOrder = ({ history, location, match }) => {
+  const [viewableTableArray, setViewableTableArray] = useState([]);
+  const [loader, setLoader] = useState(true);
+  useEffect(() => {
     const orderId = match.params.id;
-    useEffect(()=>{
-        console.log(orderId)
-    })
-    return (
-        <Layout history={history} location={location}>
-            <Row>
-                <Col>{setBreadcrumb(location.pathname)}</Col>
-            </Row>
-            <Row>
-                <Col>
-                    <section className={'table-section card'}>
-                    <Table responsive>
-                        <thead>
-                        <tr>
-                            <th>Product Id</th>
-                            <th>Product Name</th>
-                            <th>Retailer</th>
-                            <th>Ordered Quantity</th>
-                            <th>Available</th>
-                            <th>Price( $ x.xx / unit)</th>
-                            <th>Total</th>
-                            <th>Action</th>
-                        </tr>
-                        </thead>
-                        <tbody>
+    makeXHR(
+      "GET",
+      `purchase-order/get?purchaseOrder=${convertFromHex(orderId)}`
+    ).then(createViewableObject);
+  }, []);
 
-                        </tbody>
-                    </Table>
-                    </section>
+  const createViewableObject = ({ result }) => {
+    let retailerDetails = {};
+    let tempArray = viewableTableArray;
+    connectToWeb3()
+      .then(() => {
+        return getRetailerDetails(result.retailerAddress);
+      })
+      .then(obj => {
+        retailerDetails = obj;
+        let i = 0;
+        result.orders.forEach(item => {
+          i++;
+          fetchProductUnitDetailsUsingUID(item.productUnitId).then(
+            ({ details }) => {
+              let oldUnitsUsed = details.totalPacketsUsed
+                ? details.totalPacketsUsed
+                : 0;
+              let left = details.totalPacketsManufactured - oldUnitsUsed;
+              let objToAdd = {
+                productId: item.productUnitId,
+                productName: details.productName,
+                orderedQuantity: item.amount,
+                available: left,
+                price: details.distributorToRetailerPrice
+              };
+              console.log(objToAdd);
+              tempArray.push(objToAdd);
+              setViewableTableArray([...tempArray]);
+              if (i === result.orders.length) {
+                setLoader(false);
+              }
+            }
+          );
+        });
+      });
+  };
 
-                </Col>
-            </Row>
-        </Layout>
-    );
+  return (
+    <>
+      <Layout history={history} location={location}>
+        <Row>
+          <Col>{setBreadcrumb(location.pathname)}</Col>
+        </Row>
+        <Row>
+          <Col>
+            <section className={"table-section card"}>
+              <Table responsive>
+                <thead>
+                  <tr>
+                    <th>Product Id</th>
+                    <th>Product Name</th>
+                    <th>Retailer</th>
+                    <th>Ordered Quantity</th>
+                    <th>Available</th>
+                    <th>Price( $ x.xx / unit)</th>
+                    <th>Total</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
+                <tbody></tbody>
+              </Table>
+            </section>
+          </Col>
+        </Row>
+      </Layout>
+      {loader ? <Loader message={"Fetching Data..."} /> : null}
+    </>
+  );
 };
 
 export default PurchaseOrder;
