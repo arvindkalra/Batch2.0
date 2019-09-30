fdc73207dc66b1fba292ac5a6958fc34303ebfa0
arvindkalra <kalarvind97@gmail.com>
2019-09-30 14:49:13 +0530

    all orders fetched by retailer and distributor

--- src/components/retailer/AllPurchaseOrders.js
+++ src/components/retailer/AllPurchaseOrders.js
@@ -9,82 +9,23 @@ import { Link } from "react-router-dom";
 const AllPurchaseOrders = ({ noPurchaseOrder, forRetailer }) => {
   const [tableRows, setTableRows] = useState([]);
 
-  //TODO address to be passed is supposed to be distributor, but passing OWN_ADDRESS since there is no such api endpoint to handle this situation
   useEffect(() => {
     connectToWeb3().then(() => {
-      makeXHR("GET", `order/get/all?address=${OWN_ADDRESS}`).then(
-        ({ result }) => findPurchaseOrdersForRetailer(result)
-      );
-    });
-  }, []);
-
-  const findPurchaseOrdersForRetailer = result => {
-    let tempTableRows = tableRows;
-    let purchaseOrders = [];
-    if (result.length === 0) {
-      console.log("No Purchase Order");
-      noPurchaseOrder();
-      return;
-    }
-    // Find purchase id's for this retailer
-    result.forEach(element => {
-      if (
-        (forRetailer && element.retailerAddress === OWN_ADDRESS) ||
-        !forRetailer
-      ) {
-        if (
-          purchaseOrders.findIndex(ele => element.purchaseOrderId === ele) ===
-          -1
-        ) {
-          purchaseOrders.push(element.purchaseOrderId);
-        }
+      if (forRetailer) {
+        makeXHR("GET", `getAllOrders?retailerAddress=${OWN_ADDRESS}`).then(
+          ({ result }) => {
+            setTableRows([...result]);
+          }
+        );
+      } else {
+        makeXHR("GET", `getAllOrders?distributorAddress=${OWN_ADDRESS}`).then(
+          ({ result }) => {
+            setTableRows([...result]);
+          }
+        );
       }
     });
-
-    purchaseOrders.forEach(orderId => {
-      makeXHR("GET", `purchase-order/get?purchaseOrder=${orderId}`).then(
-        ({ result }) => {
-          let numOrders = result.orders.length;
-          let purchaseOrderId = convertToHex(orderId);
-          let orderTime = result.orders[0].orderDate;
-          let status = "";
-          let pending = false;
-          let completed = false;
-          for (let i = 0; i < result.orders.length; i++) {
-            let order = result.orders[i];
-            if (pending && completed) break;
-            if (order.currentState === "Pending") {
-              pending = true;
-            } else if (order.currentState === "Completed") {
-              completed = true;
-            }
-          }
-
-          if (pending && completed) {
-            status = "Partially Completed";
-          } else if (pending) {
-            status = "Pending";
-          } else {
-            status = "Completed";
-          }
-
-          let objToAdd = {
-            numOrders,
-            status,
-            orderTime,
-            purchaseOrderId,
-            numOrderId: orderId
-          };
-          tempTableRows.push(objToAdd);
-          tempTableRows.sort((a, b) => {
-            return b.numOrderId - a.numOrderId;
-          });
-          setTableRows([...tempTableRows]);
-        }
-      );
-    });
-  };
-
+  }, []);
   return (
     <>
       <Table responsive>
@@ -103,21 +44,27 @@ const AllPurchaseOrders = ({ noPurchaseOrder, forRetailer }) => {
                 <td>
                   {forRetailer ? (
                     <span className={"under-linked"}>
-                      <Link to={`/retailer/order/${element.purchaseOrderId}`}>
-                        {element.purchaseOrderId}
+                      <Link
+                        to={`/retailer/order/${convertToHex(element.orderId)}`}
+                      >
+                        {convertToHex(element.orderId)}
                       </Link>
                     </span>
                   ) : (
-                      <span className={'under-linked'}>
-                      <Link to={`/distributor/order/${element.purchaseOrderId}`}>
-                    {element.purchaseOrderId}
+                    <span className={"under-linked"}>
+                      <Link
+                        to={`/distributor/order/${convertToHex(
+                          element.orderId
+                        )}`}
+                      >
+                        {convertToHex(element.orderId)}
                       </Link>
-                          </span>
+                    </span>
                   )}
                 </td>
-                <td>{element.numOrders}</td>
-                <td>{element.orderTime}</td>
-                <td>{element.status}</td>
+                <td>{element.totalItems}</td>
+                <td>{element.time}</td>
+                <td>{element.orderStatus}</td>
               </tr>
             );
           })}
